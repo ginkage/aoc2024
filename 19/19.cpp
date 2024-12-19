21e500b6bd6e2d424488bc2b836c7becfe390456
Ivan Podogov <ginkage@yandex.ru>
2024-12-19 09:34:10 +0000

    Plant some pretty trees

--- 19/19.cpp
+++ 19/19.cpp
@@ -1,51 +1,53 @@
 #include "../lib.hpp"
 
-const char *col = "wubrg";
-unordered_set<uint32_t> towel;
+struct node_t {
+    unordered_map<char, unique_ptr<node_t>> child;
+    bool leaf { false };
+};
+node_t root;
 
 long check(string &d, vector<long> &ways, int k) {
     if (ways[k] >= 0)
         return ways[k];
 
     long w = 0;
-    int32_t t = 0;
-    for (int len = 1; k + len <= d.size() && len <= 8; len++) {
-        t = (t << 3) + (strchr(col, d[k + len - 1]) - col + 1);
-        if (towel.find(t) != towel.end())
-            w += check(d, ways, k + len);
+    node_t *node = &root;
+    for (int j = k; j < d.size(); j++) {
+        auto it = node->child.find(d[j]);
+        if (it == node->child.end()) break;
+        node = it->second.get();
+        if (node->leaf)
+            w += check(d, ways, j + 1);
     }
 
-    ways[k] = w;
-    return w;
+    return (ways[k] = w);
 }
 
 int main() {
-    long result = 0;
     string s;
-
     getline(cin, s);
-    auto pattern = split_s(s, ", ");
-    getline(cin, s);
-
-    for (string &s : pattern) {
-        int32_t t = 0;
-        for (char c : s)
-            t = (t << 3) + (strchr(col, c) - col + 1);
-        towel.insert(t);
+    for (string &p : split_s(s, ", ")) {
+        node_t *node = &root;
+        for (char c : p) {
+            if (!node->child[c].get())
+                node->child[c] = make_unique<node_t>();
+            node = node->child[c].get();
+        }
+        node->leaf = true;
     }
+    getline(cin, s);
 
+    long result_a = 0, result_b = 0;
     while (true) {
-        string s;
         getline(cin, s);
         if (!cin) break;
-
         vector<long> ways(s.size() + 1, -1);
         ways[s.size()] = 1;
-
-        result += check(s, ways, 0);
+        long w = check(s, ways, 0);
+        result_a += w > 0;
+        result_b += w;
     }
-
-    cout << result << endl;
+    cout << result_a << ' ' << result_b << endl;
 
     return 0;
 }
