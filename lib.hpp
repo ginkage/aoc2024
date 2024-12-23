95f926f69f9386d9536dfa4641976e5ab7c92888
Ivan Podogov <ginkage@yandex.ru>
2024-12-23 11:27:07 +0000

    Extra headers

--- lib.hpp
+++ lib.hpp
@@ -1,11 +1,13 @@
 #include <algorithm>
 #include <cmath>
+#include <fstream>
 #include <iostream>
 #include <queue>
 #include <regex>
 #include <set>
 #include <sstream>
 #include <string>
+#include <unordered_map>
 #include <unordered_set>
 #include <vector>
 #include <unistd.h>
